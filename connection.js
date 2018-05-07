2c5c313051a429816aab08443b2cc35507b018d5
yhoshua.ochoa <yhoshua.ochoa@sngular.team>
2018-05-07 18:01:14 -0500

    complete crud

--- connection.js
+++ connection.js
@@ -1,53 +1,152 @@
 const MongoClient = require('mongodb').MongoClient;
-const objectID = require('mongodb').ObjectId;
+const ObjectId = require('mongodb').ObjectId;
 
 const SERVER_ADDR = 'mongodb://localhost:27017';
-const DATABASE  = 'baseprueba';
+const DATABASE  = 'heros';
 
 const SERVER_ERROR = {
   status:500,
   data:{message:'Mongodb connection error X('}
 };
 
-const get = function(dbCollection) {
+
+//Funcion obtener documento por id
+const getOne = function(dbCollection, id) {
+
   return new Promise((resolve, reject) => {
     MongoClient.connect(SERVER_ADDR, (server_err, client) => {
+
       if (server_err !== null){
         reject(SERVER_ERROR);
       };
       const db = client.db(DATABASE);
 
-      db.collection(dbCollection).find({}).toArray( (findErr, documents) => {
-        if (findErr !== null) {
-          reject({status: 400, data:{ message: 'Error to find documents'}});
-          client.close()
-        };
+      db.collection(dbCollection).findOne({ _id: new ObjectId(id) }, (err, result) => {
 
-        resolve({
-          status: 200,
-          data: documents
-        });
-        client.close();
+      resolve({
+        status: 200,
+        data: result
+       });
+
+      client.close();
+
       });
-    });
+    });//cierre MongoClient
   });
 }
 
-const put = function(dbCollection){
+//Funcion obtener todos los documentos
 
-  return new Promise((resolve, reject) =>{
-    MongoClient.connect(SERVER_ADDR, (server_err, client)=>{
+const get = function(dbCollection){
+   return new Promise((resolve, reject) => {
+      MongoClient.connect(SERVER_ADDR, (server_err, client) => {
+         if(server_err != null){
+           reject(SERVER_ERROR);
+         }
+
+         const db = client.db(DATABASE);
+
+         db.collection(dbCollection).find({}).toArray((findErr, documents) => {
+           if(findErr !== null){
+             reject({status:400, data:{message: 'Error to find documents'}});
+             client.close();
+           }
+
+           resolve({
+             status:200,
+             data:documents
+           });
+
+           client.close();
+         });
+      });
+   });
+}
+
+//Funcion para insertar documento
+const post = function(dbCollection, data){
+
+  return new Promise((resolve, reject) => {
+     MongoClient.connect(SERVER_ADDR, (server_err, client) => {
+        if(server_err !== null){
+          reject(SERVER_ERROR);
+        }
+
+        const db = client.db(DATABASE);
+
+        db.collection(dbCollection).insert(data, (insertErr, result) => {
+
+          if(insertErr !== null){
+            reject({
+              status:400,
+              data:{message:'Error to save in databse'}
+            });
+          }
+           resolve({
+             status:200,
+             data:result
+           });
+           client.close();
+        });
+     });
+  });
+}
+
+//Funcion para actualizar documento
+const put = function(dbCollection, id, data){
+   return new Promise((resolve, reject) => {
+      MongoClient.connect(SERVER_ADDR, (server_err, client) =>{
+        if(server_err !== null){
+          reject(SERVER_ERROR);
+        }
+
+        const db = client.db(DATABASE);
+
+        db.collection(dbCollection).update({_id: new ObjectId(id)}, data,
+         (updateErr, result) => {
+            if(updateErr !== null){
+
+              console.log(updateErr);
+              reject({
+                status:400,
+                data:{message:'Unable to update user'}
+              });
+            }
+
+            resolve({status:200, data:null});
+            client.close();
+         });
+      });
+   });
+}
+
+//funcion ára borrar documentos
+const deleteById = function(dbCollection, id){
+  return new Promise((resolve, reject) => {
+    MongoClient.connect(SERVER_ADDR, (server_err, client) => {
        if(server_err !== null){
-         reject();
-         client.close();
+         reject(SERVER_ERROR);
        }
-
        const db = client.db(DATABASE);
 
-       // const collection = db.collection('documents');
-       //db.collection(dbCollection).insert();
+       db.collection(dbCollection).deleteOne({ _id: new ObjectId(id)}, (delateErr, result) => {
+         if(delateErr !== null){
 
+           reject({
+             status:404,
+             data:{message:'Unable to delete or not found'}
+           });
 
+           client.close();
+         }
+
+         resolve({
+           status:204,
+           data:result
+         });
+
+         client.close();
+       });
     });
   });
 
@@ -56,5 +155,9 @@ const put = function(dbCollection){
 
 
 module.exports = {
+  getOne,
   get,
+  post,
+  put,
+  delete:deleteById
 };
--- crud.js
+++ crud.js
@@ -11,13 +11,49 @@ app.get('/', (req, res) =>{
   res.send("PROBANDO...");
 });
 
-app.get('/datos', (req, res) => {
-  connection.get('datos').then(
+//GEt por id
+app.get('/heros/:userId', (req, res) => {
+  connection.getOne('heros', req.params.userId).then(
     result => {res.status(result.status).send(result.data);}
-  ).catch(err=>{
+  ).catch(err => {
      res.status(err.status).send(err.data);
   });
 });
 
+//GET que trae todos los campos
+app.get('/heros', (req, res) => {
+  connection.get('heros').then(
+    result => {res.status(result.status).send(result.data);}
+  ).catch(err => {
+    res.status(err.status).send(err.data);
+  });
+});
+
+//POST para insertar un elemento
+app.post('/heros', (req, res) =>{
+   connection.post('heros', req.body).then(
+     result => {res.status(result.status).send(result.data)}
+   ).catch(err =>{
+     res.status(err.status).send(err.data);
+   });
+});
+
+//Actualizar por id
+app.put('/heros/:userId', (req, res) => {
+  connection.put('heros', req.params.userId, req.body).then(
+    result => {res.status(result.status).send(result.data)}
+  ).catch(err => {
+      res.status(err.status).send(err.data);
+  });
+});
+
+//Borrar por id
+app.delete('/heros/:userId', (req, res) => {
+  connection.delete('heros', req.params.userId).then(
+    result => {res.status(result.status).send(result.data);}
+  ).catch(err => {
+    res.status(err.status).send(err.data);
+  });
+});
 
 app.listen(3001, ()=> console.log("Corrriendo en puerto 3001"));
